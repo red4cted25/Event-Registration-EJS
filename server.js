17c3d76fb4f7b079a224cffec8cd79816a44cdc4
Nico <141285419+red4cted25@users.noreply.github.com>
2024-09-13 15:21:50 -0700

    Finished Layouts and Routing
    Completed layouts and routing information to the index page

--- server.js
+++ server.js
@@ -1,17 +1,46 @@
 var express = require('express')
 var app = express()
+const fs = require('fs')
 const PORT = 5000
+const expressLayouts = require('express-ejs-layouts');
+
+// Middleware
+app.use(expressLayouts);
+app.set('layout', 'layouts/main');
+// express.static
+app.use('/public', express.static('public'));
 
 // Set the view engine to ejs
 app.set('view engine', 'ejs')
 
-// use res.render to load up an ejs view file
-// index page
-// app.get('/', (req, res) => {
-//     res.render('pages/index', {
-//         events: events,
-//     })
-// })
+// Read data from a JSON file
+let data = fs.readFileSync('data/events.json');
+let jsonData = JSON.parse(data)
+
+// Index page
+app.get('/', (req, res) => {
+    res.render('pages/index', {
+        events: jsonData
+    })
+})
+
+// // Read data from a JSON file
+// let data = fs.readFileSync('/data/json');
+// let jsonData = JSON.parse(data)
+
+// console.log(jsonData)
+
+// // Write data to a JSON file
+// let testEvent = {
+//     name: "testEvent", attendees: 25, type:"outdoor party"
+// }
+
+// // Convert object to JSON string
+// let newJsonData = JSON.stringify(testEvent, null, 2)
+
+// //Write the JSON data to a file
+// fs.writeFileSync('/data/json', newJsonData)
+
 
 // register page
 // app.get('/register', (req, res) => {
