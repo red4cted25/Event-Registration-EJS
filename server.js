b905a92a5d6effea2eef79983e6331186e700de0
Nico <ndiaz413@west-mec.org>
2024-09-16 01:12:27 -0700

    Updated Middleware and Routing
    Updated the middleware for POSTing and began work on app.post for the register page

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const expressLayouts = require('express-ejs-layouts');
 // Middleware
 app.use(expressLayouts);
 app.set('layout', 'layouts/main');
-// express.static
+// express.static allows css/js to be used in files without MIME error
 app.use('/public', express.static('public'));
+// express.urlencoded ({extended:true}) allows req.body to be taken from forms with POST 
+app.use(express.urlencoded({ extended: true }));
 
 // Set the view engine to ejs
 app.set('view engine', 'ejs')
@@ -48,30 +50,12 @@ app.get('/register', (req, res) => {
     });
 });
 
-// // Read data from a JSON file
-// let data = fs.readFileSync('/data/json');
-// let jsonData = JSON.parse(data)
+app.post('/register', (req, res) => {
+    const { name, email, eventId } = req.body; // Gets event id from key/value pairs in url
+});
 
-// console.log(jsonData)
-
-// // Write data to a JSON file
-// let testEvent = {
-//     name: "testEvent", attendees: 25, type:"outdoor party"
-// }
-
-// // Convert object to JSON string
-// let newJsonData = JSON.stringify(testEvent, null, 2)
-
-// //Write the JSON data to a file
+//Write the JSON data to a file
 // fs.writeFileSync('/data/json', newJsonData)
-
-
-// register page
-// app.get('/register', (req, res) => {
-//     res.render('pages/register', {
-//         events: events,
-//     })
-// })
 
 // admin pages
 // app.get('/admin', (req, res) => {
