ba283b32b534b1a4947d72c2d2f4505fe85e6e16
Nico <ndiaz413@west-mec.org>
2024-09-17 00:02:39 -0700

    Update Alert Function
    Updated the alert function to occur only when all DOM content has been loaded

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
-// Check if the URL has the "success" parameter
-const urlParams = new URLSearchParams(window.location.search);
-if (urlParams.has('success')) {
-    // Show a success alert if the parameter exists
-    alert('Your data was uploaded successfully!');
-}
+document.addEventListener('DOMContentLoaded', () => {
+    // Check if the URL has the "success" parameter
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has('success')) {
+        // Show a success alert if the parameter exists
+        alert('Your data was uploaded successfully!');
+    }
+});
